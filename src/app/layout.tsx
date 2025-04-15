5b37df37cd0e920e335e923320c9c321075967d0
Seu Nome <seu.email@exemplo.com>
2025-04-15 20:28:34 -0300

    fix: adiciona tipo Metadata ao objeto metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export const metadata = {
+export const metadata: Metadata = {
   title: 'Postura Hacker',
   description: 'Comunidade. Podcast. Newsletter.',
   openGraph: {
